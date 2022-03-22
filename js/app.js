5fccdf7d640d527a386e54de416d8bd8a394ea96
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-03-22 20:44:38 +0200

    msking nice

--- js/app.js
+++ js/app.js
@@ -43,10 +43,68 @@ let navbar = document.querySelector('.navbar');
 window.addEventListener('load',()=>{
     let navH = navbar.getBoundingClientRect().height;
     let scrHi = window.pageYOffset;
-    console.log(scrHi)
     if(scrHi > navH){
         navbar.classList.add('fixed-nav')
     }else{
         navbar.classList.remove('fixed-nav')
     }
 })
+
+
+function coutdown(){
+    // time out
+let myDays = document.getElementById('days')
+let myHours = document.getElementById('hours')
+let myMinutes = document.getElementById('minutes')
+let mySeconds = document.getElementById('seconds')
+// console.log(myDays)
+
+let future = new Date('jun 20, 2022').getTime();
+let now = new Date().getTime();
+let gap = future - now;
+
+// assinging the values into millli seconds
+let second = 1000;
+let minute = second * 60;
+let hour = minute * 60;
+let day = hour * 24;
+
+// dividing into pieces 
+let d = Math.floor(gap / day);
+let h = Math.floor((gap % day) / hour);
+let m = Math.floor((gap % hour) / minute);
+let s = Math.floor((gap % minute) / second);
+// console.log(s)
+
+// ku buuxinta days into 0 
+if(d < 10){
+    d = '0'+d
+}else{
+    d = d
+}
+// ku buuxinta hours into 0 
+if(h < 10){
+    h = '0'+h
+}else{
+    h = h
+}
+// ku buuxinta minutes into 0 
+if(m < 10){
+    m = '0'+m
+}else{
+    m = m
+}
+// ku buuxinta second into 0 
+if(s < 10){
+    s = '0'+s
+}else{
+    s = s
+}
+myDays.innerHTML = `${d}:`;
+myHours.innerHTML = `${h}:`;
+myMinutes.innerHTML = `${m}:`;
+mySeconds.innerHTML = s;
+
+}
+setInterval(coutdown, 100);
+
